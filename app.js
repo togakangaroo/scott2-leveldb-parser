517489e0227c6d07c75d6ef9c451c68f5b0b3c58
George Mauer <gmauer@gmail.com>
2017-02-05 15:06:55 -0600

    form is working

--- app.js
+++ app.js
@@ -1,46 +1,51 @@
-const gettingRecords = require('./data')
-
-const rand = (max, min = 0) => Math.floor((max + min) * Math.random() - min)
-const pickRandom = (count, arr) =>
-      count <= 0 ? [] : arr.splice(rand(arr.length), 1).concat(pickRandom(count-1, arr))
-
 const express = require('express')
 const app = express()
+const gettingRecords = require('./data')
+const uniq = require('lodash/uniq')
+const pick = require('lodash/pick')
+require('csv-express')
 
 app.set('view engine', 'pug')
 
+const uniqueKeysOfAll = objs =>
+    uniq(objs.map(Object.keys).reduce((prev, keys) => prev.concat(keys), []))
+const gettingRecordData = gettingRecords.then(records => {
+    return {
+        records,
+        properties: uniqueKeysOfAll(records)
+    }
+})
 app.get('/', (req, res) => {
-    gettingRecords.then(records => {
+    gettingRecordData.then(({records, properties}) => {
         res.render('index', {
-            title: 'Hey',
             message: `There are ${records.length} records`,
-            properties: Object.keys(records[0]),
+            properties
         })
     })
 })
 
-app.listen(8121, () => {
-    console.log('Example app listening on http://localhost:8121')
+app.get('/csv', (req, res) => {
+    const query = req.query
+    const startDate = Date.parse(query.startDate)
+    const endDate = Date.parse(query.endDate)
+    const desiredColumns = Object.keys(query.columns).filter(name => query.columns[name] === 'on')
+    gettingRecordData
+        .then(({records}) =>
+            records.filter(r => {
+                const d = Date.parse(r.post_date)
+                return startDate <= d && d <= endDate
+            })
+        )
+        .then((records) => records.map(r => pick(r, desiredColumns)))
+        .then(records => {
+            console.log(`Outputing ${records.length}`)
+            res.csv([
+                uniqueKeysOfAll(records),
+                records.map(Object.values)
+            ])
+        }).then(null, (err) => console.error(err))
 })
 
-// const records = []
-// db.createReadStream({ keys: true, values: true })
-//     .on('data', ({key, value}) => {
-//         try {
-//             records.push(JSON.parse(value))
-//         } catch(e) {
-//             console.error(e)
-//         }
-//     })
-//     .on('end', () => {
-//         console.log(`All data read in`)
-//     })
-
-// const showRandomRecords = () => {
-//     const objsToShow = pickRandom(5, records)
-//     for(let obj of objsToShow) {
-//         for(let k of Object.keys(obj))
-//             console.log(`${k}\t\t${obj[k]}`)
-//         console.log(`-----------------\n`)
-//     }
-// }
+app.listen(8121, () => {
+    console.log('App listening on http://localhost:8121')
+})
